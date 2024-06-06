189d5e7f7258e37a73beaa15d504164267593405
unknown <support@stargroup.al>
2024-06-06 16:00:11 +0200

    First commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
+
+const Footer = () => {
+  return <div>Copyright © 2024 Stargroup All rights reserved.</div>;
+};
+
+export default Footer;
